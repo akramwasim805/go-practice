103d14de96cc1a66e6b650a0443ef078e370c7d8
Wasim Akram <wasimakram@Wasims-MacBook-Pro.local>
2023-08-02 00:20:05 +0530

    Adding chapter 3

--- mygolang-lco/03userinput/main.go
+++ mygolang-lco/03userinput/main.go
@@ -1,7 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
 
 func main() {
 	fmt.Println("We are testing third chapter here named as user input")
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Println("Enter the ratting of the Pizza: ")
+
+	// commma ok || err ok
+
+	input, _ := reader.ReadString('\n')
+
+	fmt.Println("Thanks for rating, ", input)
+	fmt.Printf("Type of this rating is %T", input)
+
 }
